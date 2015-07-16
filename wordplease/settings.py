55f606f63b4a14202ccada5c6c58e1a1b928771c
Javi Alzueta <jalzuetap@gmail.com>
2015-07-16 16:53:15 +0200

    Creadas las Aplicaciones para 'posts', 'categorices' y 'users' y añadidas a settings

--- wordplease/settings.py
+++ wordplease/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cathegories',
+    'posts',
+    'users'
 )
 
 MIDDLEWARE_CLASSES = (
