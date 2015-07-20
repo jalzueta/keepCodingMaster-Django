afad349dcfe721585f7a353377a96f63f2334360
Javi Alzueta <jalzuetap@gmail.com>
2015-07-20 11:01:16 +0200

    Añadido base.html para el proyecto, con menu, cabecera y pie Añadido template 'home.html' e implementado template para un post (post_loop.html), que se reutiliza dentro de home.html Añadida url para la home. Creada clase HomeView, utilizando vista generica 'View'

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import View, ListView
+from posts.settings import PUBLICADO
+from posts.models import Post
 
 
-def home(request):
-    return HttpResponse('Hola Mundo')
+class HomeView(View):
+    """
+    Esta funcion devuelve el 'home' de mi pagina web
+    """
+    def get(self, request):
+        posts = Post.objects.filter(status=PUBLICADO).order_by('-publication_data')
+        context = {
+            'posts_list': posts[:5]
+        }
+        return render(request, 'posts/home.html', context)
+
--- wordplease/urls.py
+++ wordplease/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from posts.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'posts.views.home')
+    url(r'^$', HomeView.as_view(), name='posts_home'),
 ]
